5fda861820f387c4978361177b8a983aa37f83da
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-05-29 01:02:53 +0600

    check data retendency

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ async function run() {
     app.post('/users',async(req,res)=>{
       const user =req.body;
       console.log(user);
+      const query = {email: user.email};
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: "User Already Exist"});
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
